8027fbb4c3188f125a57680b09d60077c8a6f955
harmony365 <moisesramos@harmony365.es>
2023-11-28 13:07:30 +0100

    Implementación de CategoryController.

--- products/src/main/java/com/microservices/products/controller/CategoryController.java
+++ products/src/main/java/com/microservices/products/controller/CategoryController.java
@@ -1,22 +1,40 @@
 package com.microservices.products.controller;
 
-
 import lombok.Getter;
 import lombok.Setter;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/api/v1/categories")
 @Getter
 @Setter
+@RefreshScope
 public class CategoryController {
-    @Value("${app.testProp}")
-    private String testProp;
 
-    public String getTestProp() {
-        return this.testProp;
+    @Value(value = "${app.testProp}")
+    private String appTestProp;
+
+    @Value(value = "${boot.app.testProp}")
+    private String bootTestProp;
+
+    @Value(value = "${git.app.testProp}")
+    private String gitTestProp;
+
+    @GetMapping("/app-test-prop")
+    public String getAppTestProp() {
+        return this.appTestProp;
     }
+    @GetMapping("/boot-test-prop")
+    public String getBootTestProp() {
+        return this.bootTestProp;
+    }
+    @GetMapping("/git-test-prop")
+    public String getGitTestProp() { return this.gitTestProp; }
+
 }
